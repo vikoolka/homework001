88b4b0abac6a2e2faab66dc361270609f5c9b5a0
Vika <vir.89@yandex.ru>
2023-08-03 21:05:19 +0500

    Homework009

--- Homework009/Program.cs
+++ Homework009/Program.cs
@@ -51,3 +51,17 @@ Console.WriteLine(SumNumber(num1, num2));
 m = 2, n = 3 -> A(m,n) = 9
 m = 3, n = 2 -> A(m,n) = 29
 */
+
+int Ackermann(int m, int n)
+    {
+        if (m == 0) return n + 1;
+        else if (m > 0 && n == 0) return Ackermann(m - 1, 1);
+        else if (m > 0 && n > 0) return Ackermann(m - 1, Ackermann(m, n - 1));
+        return 0; 
+    }
+    
+Console.Write("input num1: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("input num2: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(Ackermann(num1, num2));
